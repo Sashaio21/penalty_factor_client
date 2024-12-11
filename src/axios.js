f67c202c7e3ecca955dd28b1be1968855e746780
Sascha <your.email@example.com>
2024-12-12 00:52:40 +0300

    gg

--- src/axios.js
+++ src/axios.js
@@ -4,7 +4,7 @@ const intance = axios.create({
     baseURL: 'https://penalty-factor-server.onrender.com'
     // baseURL: 'http://localhost:4444'
 });
-
+ 
 
 intance.interceptors.request.use((config)=>{
     config.headers.authorization = window.localStorage.getItem('token');
